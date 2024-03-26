9e69946d5c7fc02b63275f76450dde38d613afb7
Ryan Owens <ryanvader82@gmail.com>
2024-03-25 21:26:30 -0500

    feat: impl second stage with accept and read

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-// Uncomment this block to pass the first stage
-use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+use std::str;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() {
     eprintln!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("0.0.0.0:6379").await?;
+    let listener = TcpListener::bind("0.0.0.0:6379").await.expect("cannot listen on port 6379");
 
     loop {
-        let (socket, _) = listener.accept().await?;
-        process_request(socket).await;
+        let (socket, _) = listener.accept().await.expect("cannot accept connections");
+        tokio::spawn(async move {
+            process_request(socket).await;
+        });
     }
-
-    Ok(())
 }
 
-async fn process_request<T>(socket: T) {
-    println!("accepted new connection");
+
+async fn process_request(mut socket: TcpStream) {
+    eprintln!("accepted new connection");
+
+    let mut buf = vec![0; 16];
+    let n = socket.read(&mut buf).await.expect("cannot read from socket");
+    if n == 0 {
+        return;
+    }
+
+    let s = match str::from_utf8(&buf) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    eprintln!("received {:?}", s);
+
+    if s.to_lowercase().contains("ping") {
+        socket.write_all(b"+PONG\r\n").await.expect("cannot write pong response");
+    }
+
+    socket.flush().await.expect("cannot flush");
+    socket.shutdown().await.expect("cannot shutdown");
 }
